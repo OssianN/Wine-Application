d1067ada26a6e47e9e89e613904a6c91548b0a9d
Ossian Nörthen <ossian.northen@bimobject.com>
2024-10-06 16:41:42 +0200

    removes archived wines from storage data

--- lib/getStorageData.ts
+++ lib/getStorageData.ts
@@ -1,17 +1,19 @@
 import type { Wine } from '@/types';
 
 export const getStorageData = (wineList: Wine[]) => {
-  const { totalCost, totalYear } = wineList.reduce(
-    (acc, wine) => {
-      const winePrice = !isNaN(wine.price) ? Number(wine.price) : 0;
-      const wineYear = !isNaN(wine.year) ? Number(wine.year) : 0;
-      return {
-        totalCost: acc.totalCost + winePrice,
-        totalYear: acc.totalYear + wineYear,
-      };
-    },
-    { totalCost: 0, totalYear: 0 }
-  );
+  const { totalCost, totalYear } = wineList
+    .filter(wine => !wine.archived)
+    .reduce(
+      (acc, wine) => {
+        const winePrice = !isNaN(wine.price) ? Number(wine.price) : 0;
+        const wineYear = !isNaN(wine.year) ? Number(wine.year) : 0;
+        return {
+          totalCost: acc.totalCost + winePrice,
+          totalYear: acc.totalYear + wineYear,
+        };
+      },
+      { totalCost: 0, totalYear: 0 }
+    );
 
   const averageYear =
     totalYear / wineList.filter(wine => wine.year > 1900).length;

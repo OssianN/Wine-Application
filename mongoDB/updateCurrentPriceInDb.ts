20f05be8a6a8df27374ef90b5bad32f8c893892c
Ossian Nörthen <ossian.northen@bimobject.com>
2024-10-16 21:46:47 +0200

    fixes connection bug

--- mongoDB/updateCurrentPriceInDb.ts
+++ mongoDB/updateCurrentPriceInDb.ts
@@ -1,10 +1,13 @@
 'use server';
 import WineDataBase from './wine-schema';
+import { connectMongo } from '.';
 
-export const updateCurrentPriceInDb = (
+export const updateCurrentPriceInDb = async (
   wineId: string,
   currentPrice: number
 ) => {
+  await connectMongo();
+
   WineDataBase.findByIdAndUpdate(wineId, {
     $set: { currentPrice },
   }).exec();

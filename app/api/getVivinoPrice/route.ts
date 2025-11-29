281a8dc233566b5376affbdf689ff62969a86019
OssianN <ossian.northen@bimobject.com>
2025-11-29 12:44:08 +0100

    Switch to browswe io

--- app/api/getVivinoPrice/route.ts
+++ app/api/getVivinoPrice/route.ts
@@ -1,33 +1,12 @@
-import { updateCurrentPriceInDb } from '@/mongoDB/updateCurrentPriceInDb';
-import { getCurrentPriceOfWine } from '@/scraping/dataExtractUtils';
-import { startBrowser } from '@/scraping/getVivinoData';
-import type { Page } from 'puppeteer-core';
+import { fetchWebsiteData } from '@/scraping/getVivinoData';
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const title = searchParams.get('title');
-  const year = searchParams.get('year');
-  const link = searchParams.get('vivinoUrl');
-  const wineId = searchParams.get('wineId');
 
-  if (!title || !year) return Response.json({ error: 'Missing title or year' });
+  if (!title) return Response.json({ error: 'Missing title or year' });
 
-  const browser = await startBrowser();
+  const data = await fetchWebsiteData(title);
 
-  const page = (await browser.newPage()) as Page;
-  await page.setUserAgent(
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
-  );
-  await page.setViewport({ width: 1920, height: 1080 });
-  await page.goto(link ?? '');
-
-  const vivinoPrice = await getCurrentPriceOfWine(page);
-
-  if (wineId && vivinoPrice) {
-    updateCurrentPriceInDb(wineId, 0);
-  }
-
-  await browser.close();
-
-  return Response.json(vivinoPrice);
+  return Response.json(data);
 }
--- scraping/getVivinoData.ts
+++ scraping/getVivinoData.ts
@@ -1,42 +1,19 @@
 'use server';
-import type { Browser, Page } from 'puppeteer-core';
-import { getCurrentPriceOfWine, getWineImg } from './dataExtractUtils';
-import { getChromiumPath } from './setupChromium';
+// import type { Browser, Page } from 'puppeteer-core';
+// import { getCurrentPriceOfWine, getWineImg } from './dataExtractUtils';
+// import { getChromiumPath } from './setupChromium';
 
 export const getVivinoData = async ({ title }: { title: string }) => {
   try {
-    const searchTitle = title.split(' ').join('+');
-    const cleanSearchTitle = searchTitle
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f’]/g, '');
-
-    const browser = (await startBrowser()) as Browser;
-
-    const searchPage = await browser.newPage();
-    await configurePageSettings(searchPage as Page);
-
-    await searchPage.goto(
-      `https://www.systembolaget.se/sortiment/?q=${cleanSearchTitle}`,
-      {
-        waitUntil: 'domcontentloaded',
-        timeout: 10000,
-      }
+    const { data } = await fetchWebsiteData(title);
+    const img = data[0].results[2].attributes.find(
+      attr => attr.name === 'srcset'
     );
-
-    await searchPage.waitForSelector('#stock_scrollcontainer', {
-      timeout: 10000,
-    });
-
-    const [img, currentPrice] = await Promise.all([
-      getWineImg(searchPage),
-      getCurrentPriceOfWine(searchPage),
-    ]);
-
-    browser.close();
+    console.log({ data: data[0].results });
 
     return {
-      img,
-      currentPrice,
+      img: img?.value,
+      currentPrice: null,
       rating: null,
       country: null,
       winePageUrl: null,
@@ -47,59 +24,53 @@ export const getVivinoData = async ({ title }: { title: string }) => {
   }
 };
 
-export const configurePageSettings = async (page: Page) => {
-  await page.setRequestInterception(true);
-  page.on('request', req => {
-    if (['image', 'stylesheet', 'font'].includes(req.resourceType())) {
-      req.abort();
-    } else {
-      req.continue();
-    }
+export const fetchWebsiteData = async (
+  title: string
+): Promise<ScrapingResponse> => {
+  const TOKEN = process.env.BROWSWER_IO_KEY;
+  const url = `https://production-sfo.browserless.io/scrape?token=${TOKEN}`;
+  const headers = {
+    'Cache-Control': 'no-cache',
+    'Content-Type': 'application/json',
+  };
+
+  const searchTitle = title.split(' ').join('+');
+  const cleanSearchTitle = searchTitle
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f’]/g, '');
+
+  const data = {
+    url: `https://www.systembolaget.se/sortiment/?q=${cleanSearchTitle}`,
+    elements: [{ selector: 'img' }],
+    waitForSelector: {
+      selector: '#stock_scrollcontainer',
+      timeout: 5000,
+    },
+  };
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data),
   });
 
-  await page.setUserAgent(
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
-  );
-
-  await page.setViewport({ width: 1920, height: 1080 });
-
-  await page.setExtraHTTPHeaders({
-    'Accept-Language': 'sv-SE,sv;q=0.9,en;q=0.8',
-    Accept:
-      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Upgrade-Insecure-Requests': '1',
-  });
+  return await response.json();
 };
 
-export const startBrowser = async () => {
-  try {
-    const isVercel = !!process.env.VERCEL_PROJECT_PRODUCTION_URL;
-    let puppeteer;
-    let launchOptions: Record<string, unknown> = {
-      headless: true,
-    };
-
-    if (isVercel) {
-      const chromium = (await import('@sparticuz/chromium-min')).default;
-      puppeteer = await import('puppeteer-core');
-      const executablePath = await getChromiumPath();
-      launchOptions = {
-        ...launchOptions,
-        args: chromium.args,
-        executablePath,
-      };
-      console.log('Launching browser with executable path:', executablePath);
-    } else {
-      puppeteer = await import('puppeteer');
-    }
-
-    return await puppeteer.launch(launchOptions);
-  } catch (error) {
-    const errorMessage =
-      error instanceof Error ? error.message : 'Unknown error';
-    throw new Error(`Browser launch failed: ${errorMessage}`);
-  }
+type ScrapingResponse = {
+  data: {
+    results: {
+      attributes: {
+        name: string;
+        value: string;
+      }[];
+      height: number;
+      html: string;
+      left: number;
+      text: string;
+      top: number;
+      width: number;
+    }[];
+    selector: string;
+  }[];
 };
